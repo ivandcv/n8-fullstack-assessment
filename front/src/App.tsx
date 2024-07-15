d33aee81ad2c678a4835117b7bca547ca71d4940
Ivan Canales <davidivancanales@gmail.com>
2024-07-14 23:07:32 -0600

    boilerplate setup

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
+
+import { UIProvider } from '@contexts/uiContext';
+import { routeDefinitions } from '@lib/routes';
+import { MainLayout } from '@components/layouts';
+import { EmployeeListPage } from '@components/pages';
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to="/employees" />,
+      },
+      {
+        path: '/employees',
+        element: <Outlet />,
+        children: [
+          {
+            path: '/employees',
+            element: <EmployeeListPage />,
+            id: routeDefinitions.employeesList.id,
+          },
+          {
+            path: '/employees/:id',
+            element: <div>Not implemented</div>,
+            id: routeDefinitions.employeeDetails.id,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <UIProvider>
+      <RouterProvider router={appRouter} />
+    </UIProvider>
+  );
 }
 
-export default App
+export default App;
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from './App';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+const muiTheme = createTheme();
+
+root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
