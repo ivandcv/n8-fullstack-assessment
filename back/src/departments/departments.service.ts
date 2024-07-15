9d87230d8eb69f0aeeb6bac2651054f99cccb210
Ivan Canales <davidivancanales@gmail.com>
2024-07-14 18:36:53 -0600

    setup logging

--- back/src/departments/departments.service.ts
+++ back/src/departments/departments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
@@ -7,27 +7,33 @@ import { Department } from './entities/department.entity';
 
 @Injectable()
 export class DepartmentsService {
+  private readonly logger = new Logger(DepartmentsService.name);
+
   constructor(
     @InjectRepository(Department)
     private departmentsRepository: Repository<Department>,
   ) {}
 
   create(createDepartmentDto: CreateDepartmentDto) {
+    this.logger.log('Creating a new department');
     const department = this.departmentsRepository.create(createDepartmentDto);
     return this.departmentsRepository.save(department);
   }
 
   findAll() {
+    this.logger.log('Retrieving all departments');
     return this.departmentsRepository.find();
   }
 
   async findOne(id: number) {
+    this.logger.log(`Retrieving department with ID ${id}`);
     const department = await this.departmentsRepository.findOne({
       where: { id },
       relations: ['employees'],
     });
 
     if (!department) {
+      this.logger.warn(`Department with ID ${id} not found`);
       throw new NotFoundException(`Department with ID ${id} not found`);
     }
 
@@ -35,11 +41,13 @@ export class DepartmentsService {
   }
 
   async update(id: number, updateDepartmentDto: UpdateDepartmentDto) {
+    this.logger.log(`Updating department with ID ${id}`);
     await this.departmentsRepository.update(id, updateDepartmentDto);
     return this.findOne(id);
   }
 
   remove(id: number) {
+    this.logger.log(`Deleting department with ID ${id}`);
     return this.departmentsRepository.delete(id);
   }
 }
--- back/src/employees/employees.service.ts
+++ back/src/employees/employees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
@@ -7,27 +7,33 @@ import { Employee } from './entities/employee.entity';
 
 @Injectable()
 export class EmployeesService {
+  private readonly logger = new Logger(EmployeesService.name);
+
   constructor(
     @InjectRepository(Employee)
     private employeesRepository: Repository<Employee>,
   ) {}
 
   create(createEmployeeDto: CreateEmployeeDto) {
+    this.logger.log('Creating a new employee');
     const employee = this.employeesRepository.create(createEmployeeDto);
     return this.employeesRepository.save(employee);
   }
 
   findAll() {
+    this.logger.log('Retrieving all employees');
     return this.employeesRepository.find({ relations: ['department'] });
   }
 
   async findOne(id: number) {
+    this.logger.log(`Retrieving employee with ID ${id}`);
     const employee = await this.employeesRepository.findOne({
       where: { id },
       relations: ['department'],
     });
 
     if (!employee) {
+      this.logger.warn(`Employee with ID ${id} not found`);
       throw new NotFoundException(`Employee with ID ${id} not found`);
     }
 
@@ -35,11 +41,13 @@ export class EmployeesService {
   }
 
   async update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+    this.logger.log(`Updating employee with ID ${id}`);
     await this.employeesRepository.update(id, updateEmployeeDto);
     return this.findOne(id);
   }
 
   remove(id: number) {
+    this.logger.log(`Deleting employee with ID ${id}`);
     return this.employeesRepository.delete(id);
   }
 }
